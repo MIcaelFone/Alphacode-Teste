10688868987ed50b33ccb76958917dc2ab99847f
Micael Fone <micaelfone0@gmail.com>
2024-07-21 15:26:19 -0300

    Ajustando o header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <link href="{{ asset('css/header.css') }}" rel="stylesheet">
-<nav class="navbar navbar-expand-lg Navbar">
+<nav class="navbar navbar-expand-lg " style="background-color:blue; ">
   <div class="container-fluid">
     <a class="navbar-brand" href="{{url('/')}}"><img src="{{ asset('imagens/logo_alphacode.png') }}" class="imagem"><img></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,16 +8,10 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="{{url('/')}}">Cadastro</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <a class="nav-link" href="{{url('/gerenciamento')}}">Gerenciamento</a>
         </li>
       </ul>
     </div>
