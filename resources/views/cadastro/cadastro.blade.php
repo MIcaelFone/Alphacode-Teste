70a361f3c99c9571c6f9f7c5085b314accbf6558
Micael Fone <micaelfone0@gmail.com>
2024-07-21 15:25:00 -0300

    Deixando o cadastro responsivo

--- resources/views/cadastro/cadastro.blade.php
+++ resources/views/cadastro/cadastro.blade.php
@@ -5,12 +5,13 @@
         <title>Cadastro</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet"  href="{{ asset('css/cadastro.css') }}">
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>    
     </head>
     <body>
     @include('partials.header')
-    <h1 class="display-3">Cadastro</h1>
+    <div class="container text-center mt-3">
+        <h1 class="display-3 responsive-title">Cadastro</h1>
+    </div>
     <div class="container">
         <div class="content">
             <form class="row g-3" method="post">
