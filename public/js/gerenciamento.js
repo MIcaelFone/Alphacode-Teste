cb30ffc7c2743734078166f8ed0b49e6c0e5ce14
Micael Fone <micaelfone0@gmail.com>
2024-07-21 19:08:49 -0300

    Estruturando a função de receber os dados e adicionando funções para deletar  os contatos

--- public/js/gerenciamento.js
+++ public/js/gerenciamento.js
@@ -3,20 +3,21 @@ function receberDados() {
     $.ajax({
         url: 'http://localhost:8000/api/contato',
         type: 'GET',
-        success: function(data) {
+        success: function(contato) {
             let rows = '';
-            if(data.data.length > 0){
-                for (let i = 0; i < data.data.length; i++) {
+            if(contato.data.length > 0){
+                for (let i = 0; i < contato.data.length; i++) {
+                    let dataNascimento = contato.data[i].dataNascimento.split('-').reverse().join('/');
                     rows +=
                      `<tr>
-                        <td>${data.data[i].nome}</td>
-                        <td>${data.data[i].email}</td>
-                        <td>${data.data[i].telefone}</td>
-                        <td>${data.data[i].celular}</td>
-                        <td>${data.data[i].dataNascimento}</td>
-                         <td>${data.data[i].profissao}</td>
-                            <td><img src="../imagens/editar.png" alt="Editar" style="width: 20px; height: 20px;" id="editar"></td>
-                            <td><img src="../imagens/excluir.png" alt="Excluir" style="width: 20px; height: 20px;" id="excluir"></td>
+                        <td>${contato.data[i].nome}</td>
+                        <td>${contato.data[i].email}</td>
+                        <td>${contato.data[i].telefone}</td>
+                        <td>${contato.data[i].celular}</td>
+                        <td>${dataNascimento}</td>
+                        <td>${contato.data[i].profissao}</td>
+                        <td><img src="../imagens/editar.png" alt="Editar" style="width: 20px; height: 20px;" data-id="${contato.data[i].id}" class="editar"></td>
+                        <td><img src="../imagens/excluir.png" alt="Excluir" style="width: 20px; height: 20px;" data-id="${contato.data[i].id}" class="remover" data-bs-toggle="modal" data-bs-target="#exampleModal"></td>
                     </tr>`;
                 }
             }
@@ -28,4 +29,40 @@ function receberDados() {
         }
     });
 }
-receberDados()
+function editarContato(){
+  $(document).on('click','.editar',function(e){
+    console.log("editar");
+  })  
+}
+function buscarContatoRemover(){
+    $(document).on('click', '.remover', function() {
+        let id = $(this).data('id');
+        excluindoContato(id);
+    });
+}
+function buscarContatoEditar(){
+    $(document).on('click', '.editar', function() {
+        let id = $(this).data('id');
+        window.location.href = `http://localhost:8000/editar/${id}`;
+    });    
+}
+function excluindoContato(id){
+    $(document).on("click","#excluir",function(){
+        $.ajax({
+            url: `http://localhost:8000/api/contato/${id}`,
+            type: 'DELETE',
+            success: function() {
+                alert("Contato excluído com sucesso");
+                $("#exampleModal").modal('hide');
+                location.reload();
+            },
+            error: function(xhr) {
+                console.log(xhr.responseText);
+            }
+        })
+    })
+}
+         
+receberDados();
+buscarContatoRemover();
+buscarContatoEditar();
