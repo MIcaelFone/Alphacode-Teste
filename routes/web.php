aa31d944433433739561b647374b698f271a6e23
Micael Fone <micaelfone0@gmail.com>
2024-07-21 15:26:56 -0300

    Adicionando mais uma rota de view

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('cadastro.cadastro');
 });
+Route::get('/gerenciamento',function (){
+   return view('gerenciamento.gerenciamento');
+});
 
  
