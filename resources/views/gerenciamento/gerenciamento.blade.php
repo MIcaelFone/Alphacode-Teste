7c4ac8fe5d336b01e32ee1e0fc9624dfa88b7b0d
Micael Fone <micaelfone0@gmail.com>
2024-07-21 15:26:02 -0300

    Criando um blade de gerenciamento

---
+++ resources/views/gerenciamento/gerenciamento.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Cadastro</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>   
+        <script src="{{ asset('js/gerenciamento.js') }}"></script> 
+    </head>
+    <body>
+    @include('partials.header')
+    <div class="container text-center mt-3">
+        <h1 class="display-3 responsive-title">Gerenciamento</h1>
+    </div>
+        <div class="table-responsive">
+            <table class="table">
+            <thead class="table-light">
+            <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Email</th>
+                <th scope="col">Telefone</th>
+                <th scope="col">Celular</th>
+                <th scope="col">Data Nascimento</th>
+                <th scope="col">Profissão</th>
+                <th scope="col">Editar</th>
+                <th scope="col">Deletar</th>
+            </tr>
+            </thead>
+            <tbody class="table-group-divider">
+            </tbody>
+            </table>
+        </div>    
+    </body>
+</html>
